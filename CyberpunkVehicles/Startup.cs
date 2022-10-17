6d31bf734d431d7fe4284c1c4060a7fa7371f936
Tomasz-Budny <s208246@sggw.edu.pl>
2022-10-17 18:46:01 +0200

    Add wwwroot directory and setup project

--- CyberpunkVehicles/Startup.cs
+++ CyberpunkVehicles/Startup.cs
@@ -42,6 +42,8 @@ namespace CyberpunkVehicles
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CyberpunkVehicles v1"));
             }
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseHttpsRedirection();
 
