0b7d97c992718c818500c2e8e0652fe4815a4ae3
Tomasz-Budny <s208246@sggw.edu.pl>
2022-10-19 14:39:04 +0200

    Add ORM models for vehicle

---
+++ CyberpunkVehicles/Entities/Body.cs
@@ -0,0 +1,8 @@
+﻿namespace CyberpunkVehicles.Entities
+{
+    public class Body
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ CyberpunkVehicles/Entities/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿namespace CyberpunkVehicles.Entities
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        public string ImageRelativePath { get; set; }
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public string Description { get; set; }
+        public int Weight { get; set; }
+        public int HorsePower { get; set; }
+        public string TopSpeed { get; set; }
+        public int Year { get; set; }
+        
+        public int DrivetrainId { get; set; }
+        public int BodyId { get; set; }
+        public virtual Drivetrain Drivetrain { get; set; }
+        public virtual Body Body { get; set; }
+    }
+}
