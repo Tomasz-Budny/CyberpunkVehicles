4180139acd9c769ca9cdc75bdbc326b9d68c428f
Tomasz-Budny <s208246@sggw.edu.pl>
2022-10-19 16:42:42 +0200

    Add CreateVehicle Data transfer object

---
+++ CyberpunkVehicles/Services/CreateVehicleDto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace CyberpunkVehicles.Services
+{
+    public class CreateVehicleDto
+    {
+        [Required]
+        public IFormFile VehicleImage { get; set; }
+        [Required]
+        [MaxLength(25)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Group { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string Description { get; set; }
+        [Required]
+        public int DrivetrainId { get; set; }
+        [Required]
+        public int BodyId { get; set; }
+        [Required]
+        public int Weight { get; set; }
+        [Required]
+        public int HorsePower { get; set; }
+        [Required]
+        public int TopSpeed { get; set; }
+        [Required]
+        public int Year { get; set; }
+    }
+}
